6bb48b2706dc6913f01f7b9c38b3e0caa321af79
gerryorkin <gerry.orkin@gmail.com>
2026-02-13 15:57:42 +1100

    Initial Commit

---
+++ TapTime/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TapTime
+//
+//  Created by Gerry Orkin on 13/2/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TapTime/TapTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapTimeApp.swift
+//  TapTime
+//
+//  Created by Gerry Orkin on 13/2/2026.
+//
+
+import SwiftUI
+
+@main
+struct TapTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
